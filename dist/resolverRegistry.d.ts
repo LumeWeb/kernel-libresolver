1c9b882642b575f378e4e48563908d5815420350
Derrick Hammer <derrick@derrickhammer.com>
2022-08-18 18:50:54 -0400

    *Update dist

--- dist/resolverRegistry.d.ts
+++ dist/resolverRegistry.d.ts
@@ -1,8 +1,13 @@
 import { DNSResult, ResolverOptions } from "@lumeweb/resolver-common";
+import { RpcNetwork } from "@lumeweb/kernel-rpc-client";
 export declare class ResolverRegistry {
+  private _rpcNetwork;
+  constructor(network: RpcNetwork);
+  get rpcNetwork(): RpcNetwork;
   resolve(
     domain: string,
     options?: ResolverOptions,
     bypassCache?: boolean
   ): Promise<DNSResult>;
 }
+export { RpcNetwork };
--- dist/resolverRegistry.js
+++ dist/resolverRegistry.js
@@ -1,6 +1,14 @@
 import { DNS_RECORD_TYPE, resolverError, } from "@lumeweb/resolver-common";
 import { resolve } from "@lumeweb/kernel-dns-client";
+import { RpcNetwork } from "@lumeweb/kernel-rpc-client";
 export class ResolverRegistry {
+    _rpcNetwork;
+    constructor(network) {
+        this._rpcNetwork = network;
+    }
+    get rpcNetwork() {
+        return this._rpcNetwork;
+    }
     async resolve(domain, options = { type: DNS_RECORD_TYPE.DEFAULT }, bypassCache = false) {
         let result;
         try {
@@ -12,3 +20,4 @@ export class ResolverRegistry {
         return result;
     }
 }
+export { RpcNetwork };
