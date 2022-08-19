9818d17a06f80185091f8fc91cbd45a34ed32823
Derrick Hammer <derrick@derrickhammer.com>
2022-08-19 09:57:34 -0400

    *Update dist

--- dist/resolverRegistry.d.ts
+++ dist/resolverRegistry.d.ts
@@ -4,10 +4,21 @@ export declare class ResolverRegistry {
   private _rpcNetwork;
   constructor(network: RpcNetwork);
   get rpcNetwork(): RpcNetwork;
+  get resolvers(): Promise<Set<ResolverModule>>;
   resolve(
     domain: string,
     options?: ResolverOptions,
     bypassCache?: boolean
+  ): Promise<DNSResult>;
+}
+export declare class ResolverModule {
+  private resolver;
+  private domain;
+  constructor(resolver: ResolverRegistry, domain: string);
+  resolve(
+    domain: string,
+    options: ResolverOptions,
+    bypassCache: boolean
   ): Promise<DNSResult>;
 }
 export { RpcNetwork };
--- dist/resolverRegistry.js
+++ dist/resolverRegistry.js
@@ -1,6 +1,7 @@
 import { DNS_RECORD_TYPE, resolverError, } from "@lumeweb/resolver-common";
-import { resolve } from "@lumeweb/kernel-dns-client";
+import { getResolvers, resolve } from "@lumeweb/kernel-dns-client";
 import { RpcNetwork } from "@lumeweb/kernel-rpc-client";
+import { callModule } from "libkmodule";
 export class ResolverRegistry {
     _rpcNetwork;
     constructor(network) {
@@ -8,6 +9,15 @@ export class ResolverRegistry {
     }
     get rpcNetwork() {
         return this._rpcNetwork;
+    }
+    get resolvers() {
+        return getResolvers()
+            .catch(() => {
+            return new Set();
+        })
+            .then((resolvers) => {
+            return new Set(resolvers.map((resolver) => new ResolverModule(this, resolver)));
+        });
     }
     async resolve(domain, options = { type: DNS_RECORD_TYPE.DEFAULT }, bypassCache = false) {
         let result;
@@ -18,6 +28,25 @@ export class ResolverRegistry {
             return resolverError(e);
         }
         return result;
+    }
+}
+export class ResolverModule {
+    resolver;
+    domain;
+    constructor(resolver, domain) {
+        this.resolver = resolver;
+        this.domain = domain;
+    }
+    async resolve(domain, options, bypassCache) {
+        const [ret, err] = await callModule(this.domain, "resolve", {
+            domain,
+            options,
+            bypassCache,
+        });
+        if (err) {
+            return resolverError(err);
+        }
+        return ret;
     }
 }
 export { RpcNetwork };
